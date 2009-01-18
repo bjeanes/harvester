a5e4bd3f0fe9fa95f3e482568e54b00003c4aab8
Bodaniel Jeanes <me@bjeanes.com>
2009-01-18 11:35:35 +1000

    Made the rb_init file require in any bridge support files that it finds in the resources folder

--- rb_main.rb
+++ rb_main.rb
@@ -9,12 +9,19 @@
 # Loading the Cocoa framework. If you need to load more frameworks, you can
 # do that here too.
 framework 'Cocoa'
+framework 'Security'
 
 # Loading all the Ruby project files.
 dir_path = NSBundle.mainBundle.resourcePath.fileSystemRepresentation
 Dir.entries(dir_path).each do |path|
-  if path != File.basename(__FILE__) and path[-3..-1] == '.rb'
-    require(path)
+  extension =  path.split('.')[-1] rescue ''
+  file = File.join(dir_path,path)
+  if path != File.basename(__FILE__) and extension == 'rb'
+	NSLog("Requiring Ruby file %s" % path)
+    require file
+  elsif extension == 'bridgesupport'
+	NSLog("Loading bridge support file %s" % path)
+    load_bridge_support_file file
   end
 end
 
